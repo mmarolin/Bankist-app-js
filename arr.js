9b6a412879499c7f2f326753071975b031844bc8
mmarolin <matija.marolin11@gmail.com>
2022-01-30 16:29:16 +0100

    neke novega

--- arr.js
+++ arr.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 //SLICE does not mutate arr
@@ -31,3 +31,39 @@ console.log([...arr, ...arr2]);
 
 //JOIN
 console.log(letters.join(' - '));
+
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//Getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+//at method works on strings too
+console.log('matija'.at(0));
+console.log('matija'.at(-1));
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, mov] of movements.entries()) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}€.`);
+  } else {
+    console.log(`Movement ${i + 1}. You withdrew ${Math.abs(mov)}€.`);
+  }
+}
+
+console.log('----------FOR EACH --------------');
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}. You deposited ${mov}€.`);
+  } else {
+    console.log(`Movement ${i + 1}. You withdrew ${Math.abs(mov)}€.`);
+  }
+});
+*/
