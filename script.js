72b0ac6e15aa659c621b2dddd66f183f9cea2e55
mmarolin <matija.marolin11@gmail.com>
2022-01-31 17:15:54 +0100

    loan-btn

--- script.js
+++ script.js
@@ -176,3 +176,15 @@ btnClose.addEventListener('click', function (e) {
   }
   inputClosePin.value = inputCloseUsername.value = '';
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
