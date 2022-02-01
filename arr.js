db2fda3b1db066bc6a3e02674ea45709965a4062
mmarolin <matija.marolin11@gmail.com>
2022-02-01 15:23:55 +0100

    adding-dates

--- arr.js
+++ arr.js
@@ -182,4 +182,80 @@ console.log(movements);
 //Descending
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+//GENERATING ARRAYS
+
+const x = new Array(7);
+console.log(x);
+x.fill(1, 3, 5);
+console.log(x);
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const diceRoll = Array.from({ length: 100 }, (_, i) =>
+  Math.trunc(Math.random(i) * 6 + 1)
+);
+
+console.log(diceRoll);
+
+labelBalance.addEventListener('click', function (e) {
+  e.preventDefault();
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+});
+
+
+// CHallenge 4
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+
+let recommendedFood = dogs.forEach(
+  dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+console.log(dogs);
+
+//2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating ${
+    dogSarah.curFood > dogSarah.recFood ? 'too much' : 'too little'
+  } food.`
+);
+//3.
+
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooLittle);
+console.log(ownersEatTooMuch);
+
+//4.
+
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little.`);
+
+//5.
+
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
 */
